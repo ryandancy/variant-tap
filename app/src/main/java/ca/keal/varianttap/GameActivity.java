0b88f1c5540c429fb547b3e7cb53271a3de95166
Ryan Dancy <ryan@keal.ca>
2017-08-06 09:40:00 -0400

    Add + initialize image container in GameActivity
    It's called `imgs` and it's a GridLayout that will eventually contain
    ImageViews. It's initialized in GameActivity.onCreate() with a rowCount
    and columnCount generated from the `difficulty` variable. Currently
    `difficulty` is just a variable but it'll eventually be passed in the
    intent from the main menu.
    
    There are 3 values for `difficulty`: 4 (easy), 6 (normal), and 9 (hard).
    The difficulty numbers are the number of ImageViews in `imgs` at each
    difficulty. Note that if anything other than 4, 6, or 9 is passed in the
    intent (eventually), an error will be logged and the GridLayout will be
    set to 0x0, which will *probably* cause a crash.
    
    One more change: the root layout in activity_game.xml was changed to
    RelativeLayout.

--- app/src/main/java/ca/keal/varianttap/GameActivity.java
+++ app/src/main/java/ca/keal/varianttap/GameActivity.java
@@ -1,13 +1,58 @@
 package ca.keal.varianttap;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.GridLayout;
 
 public class GameActivity extends AppCompatActivity {
+  
+  /**
+   * The layout containing the ImageViews in the centre of the screen. The user taps on the
+   * ImageView they think is different than the others ('variant').
+   */
+  private GridLayout imgs;
   
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_game);
+    
+    // There are 3 difficulties: 4 (easy), 6 (normal) and 9 (hard). The numbers are the number of
+    // ImageViews in imgs -- the more ImageViews the harder it is.
+    // TODO: get difficulty from intent
+    int difficulty = 4; // TEMPORARY
+    
+    // Initialize imgs
+    
+    imgs = (GridLayout) findViewById(R.id.imgs);
+    
+    // Calculate imgs' rows/columns
+    int rows, columns;
+    switch (difficulty) {
+      case 4:
+        rows = 2;
+        columns = 2;
+        break;
+      case 6:
+        rows = 2;
+        columns = 3;
+        break;
+      case 9:
+        rows = 3;
+        columns = 3;
+        break;
+      default:
+        Log.e(getClass().getName(), "Passed unknown difficulty: " + difficulty);
+        rows = 0;
+        columns = 0;
+    }
+    
+    Log.i(getClass().getName(), "Difficulty " + difficulty + ", setting grid to "
+        + rows + "x" + columns);
+    
+    imgs.setRowCount(rows);
+    imgs.setColumnCount(columns);
   }
+  
 }
