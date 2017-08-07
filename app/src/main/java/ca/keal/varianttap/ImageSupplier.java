7461d29a38f3a7fdc048c0532a1719e58d666c79
Ryan Dancy <ryan@keal.ca>
2017-08-07 10:37:03 -0400

    Add ImageSupplier class with loading capability
    ImageSupplier is a package-private class that is responsible for loading
    and storing the normal/variant image pairs and giving a random pair when
    requested. It stores the pairs as Pair<Drawable, Drawable>, with the
    normal first. It currently loads all the images as Drawables in a load()
    method, to which the constructor delegates. Note that this means that
    if there are a lot of images, a significant amount of memory may be used
    up by the Drawables; however, it does speed up the displaying of the
    images (I think; none of this has been tested).
    
    Currently it can only load images in the constructor; eventually it'll
    have a method to return a random image pair.

---
+++ app/src/main/java/ca/keal/varianttap/ImageSupplier.java
@@ -0,0 +1,57 @@
+package ca.keal.varianttap;
+
+import android.content.res.AssetManager;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+import android.util.Pair;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Loads the normal and variant images and provides Drawables of random normal/variant pairs when
+ * requested.
+ */
+class ImageSupplier {
+  
+  // TODO if performance is bad, maybe don't load all the images but instead load their filenames
+  // TODO "packages" of images
+  
+  /**
+   * The list of (normal, variant) Drawable pairs.
+   */
+  private List<Pair<Drawable, Drawable>> imgs = new ArrayList<>();
+  
+  /**
+   * Initialize this {@link ImageSupplier} by loading the images from {@code assets}. There must be
+   * a normals/ and variants/ directory in the assets directory, and their contents must have the
+   * same names.
+   * @param assets - An {@link AssetManager}, most likely accessed via {@code getAssets()}.
+   */
+  public ImageSupplier(AssetManager assets) {
+    try {
+      load(assets);
+      Log.i(getClass().getName(), imgs.size() + " normal/variant image pairs loaded successfully.");
+    } catch (IOException e) {
+      Log.e(getClass().getName(), "Error loading images: " + e);
+      throw new RuntimeException(e); // UncheckedIOException is API Level 24, our min is 19
+    }
+  }
+  
+  private void load(AssetManager assets) throws IOException {
+    // TODO should we check that normals/ and variants/ have same contents or just trust it?
+    // Currently, if they aren't the same, the ones in normals/ will be loaded and if they aren't in
+    // variants/ an IOException will be thrown. Any extra variants will be ignored.
+    
+    String[] names = assets.list("normals");
+    
+    for (String name : names) {
+      imgs.add(Pair.create(
+          Drawable.createFromPath("normals/" + name),
+          Drawable.createFromPath("variants/" + name)
+      ));
+    }
+  }
+  
+}
