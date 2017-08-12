9f135475ffc2e6d5eae068d8a45305384e8bb36e
Ryan Dancy <ryan@keal.ca>
2017-08-12 11:49:56 -0400

    Add base PostGameActivity with large score at top
    There's now a PostGameActivity, it's got a large TextView at the top,
    @id/post_score_text.

---
+++ app/src/main/java/ca/keal/varianttap/PostGameActivity.java
@@ -0,0 +1,14 @@
+package ca.keal.varianttap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PostGameActivity extends AppCompatActivity {
+  
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_post_game);
+  }
+  
+}
