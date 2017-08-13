840989d4ec1244065de49a8d3678be18a70cea31
Ryan Dancy <ryan@keal.ca>
2017-08-13 09:13:00 -0400

    Add functionality to the play again button
    Now it starts a GameActivity, then removes the PostGameActivity from the
    stack.
    
    TODO: difficulty selection in PostGameActivity.

--- app/src/main/java/ca/keal/varianttap/PostGameActivity.java
+++ app/src/main/java/ca/keal/varianttap/PostGameActivity.java
@@ -1,8 +1,10 @@
 package ca.keal.varianttap;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 public class PostGameActivity extends AppCompatActivity {
@@ -21,6 +23,17 @@ public class PostGameActivity extends AppCompatActivity {
     
     TextView scoreText = (TextView) findViewById(R.id.post_score_text);
     scoreText.setText(String.valueOf(score));
+  }
+  
+  /**
+   * Play again - i.e. go to GameActivity.
+   * @param v - ignored
+   */
+  public void playAgain(View v) {
+    Intent intent = new Intent(this, GameActivity.class);
+    // TODO set a difficulty here
+    startActivity(intent);
+    finish(); // remove this activity from the stack so the user can't navigate back to it
   }
   
 }
