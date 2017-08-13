e72abc89945c5cb1ad239b325eb8f6674528183e
Ryan Dancy <ryan@keal.ca>
2017-08-13 08:38:10 -0400

    Set score TextView from intent in PostGameActivity
    Now @id/post_score_text gets set with the "SCORE" extra from the intent.
    Also its android:textSize is now 95sp; 100sp was causing issues on my
    testing phone.

--- app/src/main/java/ca/keal/varianttap/PostGameActivity.java
+++ app/src/main/java/ca/keal/varianttap/PostGameActivity.java
@@ -1,7 +1,9 @@
 package ca.keal.varianttap;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
 
 public class PostGameActivity extends AppCompatActivity {
   
@@ -9,6 +11,16 @@ public class PostGameActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_post_game);
+    
+    // Get score from intent, set it to the score TextView
+    
+    int score = getIntent().getExtras().getInt("SCORE", -1);
+    if (score == -1) { // score was not in the extras
+      Log.e(getClass().getName(), "Intent did not have \"SCORE\" extra!");
+    }
+    
+    TextView scoreText = (TextView) findViewById(R.id.post_score_text);
+    scoreText.setText(String.valueOf(score));
   }
   
 }
