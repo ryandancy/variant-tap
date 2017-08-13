4e48e32f76bc23d95f1f46ccdb2cc85747a8b432
Ryan Dancy <ryan@keal.ca>
2017-08-13 19:04:06 -0400

    Add best/average score tracking with shared prefs
    Add "bestScore", "totalScore", and "timesPlayed" SharedPreferences,
    which are used and updated by PostGameActivity and used for the best
    and average score displays. It's totally insecure -- the root user can
    change the shared preferences to whatever they want because it isn't
    encrypted or anything -- but that doesn't matter because whenever Google
    Play Games is eventually integrated, it won't use the values stored in
    SharedPreferences.

--- app/src/main/java/ca/keal/varianttap/PostGameActivity.java
+++ app/src/main/java/ca/keal/varianttap/PostGameActivity.java
@@ -1,6 +1,7 @@
 package ca.keal.varianttap;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -23,6 +24,44 @@ public class PostGameActivity extends AppCompatActivity {
     
     TextView scoreText = (TextView) findViewById(R.id.post_score_text);
     scoreText.setText(String.valueOf(score));
+    
+    // Get data from shared preferences
+    // The user is allowed to mess with the shared preferences, Google Play Games won't use them
+    // TODO: Google Play Games integration
+    
+    SharedPreferences prefs = getPreferences(MODE_PRIVATE); // hopefully only one prefs file needed
+    int bestScore = prefs.getInt("bestScore", 0); // TODO extract into constants? "SCORE" too?
+    int totalScore = prefs.getInt("totalScore", 0);
+    int timesPlayed = prefs.getInt("timesPlayed", 0);
+    
+    totalScore += score;
+    timesPlayed++;
+    
+    // Update UI with shared preferences data
+    
+    int average = totalScore / timesPlayed;
+    TextView averageText = (TextView) findViewById(R.id.average_score_text);
+    averageText.setText(String.valueOf(average));
+    
+    int newBest = Math.max(bestScore, score);
+    TextView bestText = (TextView) findViewById(R.id.best_score_text);
+    bestText.setText(String.valueOf(newBest));
+  
+    // Update shared preferences (and also update "New Best Score!" text's visibility)
+  
+    SharedPreferences.Editor editor = prefs.edit();
+    
+    if (score > bestScore) {
+      TextView newBestScoreText = (TextView) findViewById(R.id.new_best_score_text);
+      newBestScoreText.setVisibility(View.VISIBLE);
+      
+      editor.putInt("bestScore", score);
+    }
+    
+    editor.putInt("totalScore", totalScore);
+    editor.putInt("timesPlayed", timesPlayed);
+    
+    editor.apply();
   }
   
   /**
