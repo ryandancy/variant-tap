2135849acece165a45f16cb61a7c274b6992fd62
Ryan Dancy <ryan@keal.ca>
2017-08-18 11:45:21 -0400

    Add throwing image functionality in MainActivity
    MainActivity.throwImage() is now a method. When called, it constructs an
    ImageView with a randomly chosen image (normal or variant), generates a
    random parabolic path for the image to follow (thus giving it the
    appearance of being thrown), and also generates a random number of times
    for the image to rotate. It then puts all this into an AnimatorSet and
    plays it, removing the ImageView from its parent afterwards.
    
    The equation of a parabola in vertex form is the following:
        y = a(x - h)^2 + k
    where (h, k) is the vertex of the parabola. To generate a random
    parabola, we randomly generate h and k (called minBias and
    vertexHorizBias respectively) within ranges. We also generate a starting
    position for the image, or a y-intercept (variable: b). With this, we
    have enough information to solve for a:
        a = (b - k) / h^2
    We now have all the information we need for the parabola. The image
    follows this parabolic path.
    
    Rotation is done through generating a random starting orientation for
    the image, then generating a random number of times that the image will
    rotate. The rotation animation also either accelerates or decelerates;
    which one is chosen at random.
    
    Since the parent layout is a ConstraintLayout, moving the image is done
    through manipulating the horizontal and vertical biases. Thus, all the
    translation is on a scale from 0f to 1f.
    
    The ranges are all in floats and are stored in dimens.xml using a hack
    described here: https://stackoverflow.com/a/8780360.
    
    The ranges are:
    - vertical bias (for starting position and vertex minimum):
      @dimen/[min|max]_thrown_image_vertical_bias (currently 0.3-0.7)
    - horizontal maximum at the vertex:
      @dimen/[min|max]_thrown_image_horizontal_bias (currently 0.6-1.2)
      Note that if the vertex horizontal bias is over 1 it won't be shown.
    - number of times the image will rotate:
      @dimen/[min|max]_thrown_image_rotate_times (currently 0.3-1.8)
    
    Other resources:
    - @dimen/min_start_min_thrown_image_bias_diff - the minimum difference
      between the starting and minimum vertical biases. Currently 0.2.
    - @dimen/thrown_image_size - the width and height of thrown images.
      Currently 40dp.
    - @integer/image_throw_duration - the duration of the throwing
      animation, in ms. Currently 750.
    
    Also add Util.getFloatResource() and Util.randomFloatBetween(). The
    former is a utility for getting a float resource, using the hack
    described in the above StackOverflow link. The later is a utility for
    getting a random float between two values; abstracting this makes
    MainActivity.throwImage() less confusing.
    
    Note that throwImage() currently isn't called. It will be in a
    subsequent commit.

--- app/src/main/java/ca/keal/varianttap/MainActivity.java
+++ app/src/main/java/ca/keal/varianttap/MainActivity.java
@@ -1,14 +1,139 @@
 package ca.keal.varianttap;
 
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.TimeInterpolator;
+import android.animation.ValueAnimator;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+  
+  private Random random = null;
   
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+  }
+  
+  /**
+   * "Throw" (animate) a random image from the game across the screen. The image follows a parabolic
+   * path across the screen and rotates. This is for a cool animation for the MainActivity.
+   * @param invert if true, the image will be thrown right-to-left instead of left-to-right.
+   */
+  // TODO split up this huge method
+  private void throwImage(final boolean invert) {
+    if (random == null) random = new Random();
+    
+    // Construct the ImageView to be thrown
+    
+    final ImageView image = new ImageView(this);
+    
+    int parent = R.id.main_layout;
+    final ViewGroup parentLayout = (ViewGroup) findViewById(R.id.main_layout);
+    
+    int length = (int) getResources().getDimension(R.dimen.thrown_image_size);
+    final ConstraintLayout.LayoutParams params = new ConstraintLayout.LayoutParams(length, length);
+    
+    params.leftToLeft = parent;
+    params.rightToRight = parent;
+    params.topToTop = parent;
+    params.bottomToBottom = parent;
+    
+    image.setLayoutParams(params);
+    
+    Drawable drawable = ImageSupplier.getInstance(this).getRandomImage();
+    image.setImageDrawable(drawable);
+    image.setAdjustViewBounds(true);
+    image.setScaleType(ImageView.ScaleType.FIT_XY);
+    
+    // Generate the starting and minimum vertical biases
+    // Starting bias is the position at x=0, min bias is the min of the parabola
+    
+    float minVertBias = Util.getFloatResource(this, R.dimen.min_thrown_image_vertical_bias);
+    float maxVertBias = Util.getFloatResource(this, R.dimen.max_thrown_image_vertical_bias);
+    float minBiasDiff = Util.getFloatResource(this, R.dimen.min_start_min_thrown_image_bias_diff);
+    
+    float startBias = Util.randomFloatBetween(random, minVertBias + minBiasDiff, maxVertBias);
+    final float minBias = Util.randomFloatBetween(random, minVertBias, startBias - minBiasDiff);
+    
+    // Generate the vertex horizontal bias - the horizontal bias at the max vertical bias (the AoS)
+    
+    float minHorizBias = Util.getFloatResource(this, R.dimen.min_thrown_image_horizontal_bias);
+    float maxHorizBias = Util.getFloatResource(this, R.dimen.max_thrown_image_horizontal_bias);
+    
+    final float vertexHorizBias = Util.randomFloatBetween(random, minHorizBias, maxHorizBias);
+    
+    // Equation of the parabola is y = a(x - h)^2 + k where (h, k) is the vertex
+    // Find a (factor) by subbing in (0, b) - the y-intercept
+    // Rearranges to a = (b - k) / h^2
+    final float factor = (startBias - minBias) / (float) Math.pow(vertexHorizBias, 2);
+    
+    // Generate the number of times the image will rotate
+    
+    float minRotateTimes = Util.getFloatResource(this, R.dimen.min_thrown_image_rotate_times);
+    float maxRotateTimes = Util.getFloatResource(this, R.dimen.max_thrown_image_rotate_times);
+    float rotateTimes = Util.randomFloatBetween(random, minRotateTimes, maxRotateTimes);
+    
+    float initialRotation = Util.randomFloatBetween(random, 0, 360);
+    
+    Log.d(getClass().getName(), "Throwing image with startBias = " + startBias + ", vertex = ("
+        + vertexHorizBias + ", " + minBias + "), rotating " + rotateTimes + " times");
+    Log.d(getClass().getName(),
+        "Equation: y = " + factor + "(x - " + vertexHorizBias + ")^2 + " + minBias);
+    
+    // Construct the parabola animator, which moves the image
+    
+    ValueAnimator parabolaAnimator = ValueAnimator.ofFloat(0, 1); // TODO come from off left side
+    parabolaAnimator.setInterpolator(new LinearInterpolator());
+    parabolaAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+      public void onAnimationUpdate(ValueAnimator animation) {
+        float xBias = (float) animation.getAnimatedValue();
+        
+        // Equation is y = a(x - h)^2 + k
+        float yBias = factor * (float) Math.pow(xBias - vertexHorizBias, 2) + minBias;
+        
+        params.horizontalBias = invert ? 1f - xBias : xBias; // handle right-to-left
+        params.verticalBias = yBias;
+        image.setLayoutParams(params);
+      }
+    });
+    parabolaAnimator.addListener(new Animator.AnimatorListener() {
+      public void onAnimationStart(Animator animation) {}
+      public void onAnimationRepeat(Animator animation) {}
+      public void onAnimationCancel(Animator animation) {}
+      public void onAnimationEnd(Animator animation) {
+        parentLayout.removeView(image);
+        // hopefully image is gone now...
+      }
+    });
+    
+    // Construct the rotate animator, which rotates the image
+    ObjectAnimator rotateAnimator = ObjectAnimator.ofFloat(
+        image, ImageView.ROTATION, initialRotation, rotateTimes * 360 + initialRotation);
+    // noinspection RedundantCast - it was casting to BaseInterpolator, only available with API 22
+    rotateAnimator.setInterpolator(random.nextBoolean()
+        ? (TimeInterpolator) new AccelerateInterpolator()
+        : (TimeInterpolator) new DecelerateInterpolator());
+    
+    // Put them together and play
+    AnimatorSet throwAnim = new AnimatorSet();
+    throwAnim.play(parabolaAnimator).with(rotateAnimator);
+    throwAnim.setDuration(getResources().getInteger(R.integer.image_throw_duration));
+    parentLayout.addView(image);
+    throwAnim.start();
   }
   
 }
--- app/src/main/java/ca/keal/varianttap/Util.java
+++ app/src/main/java/ca/keal/varianttap/Util.java
@@ -1,17 +1,34 @@
 package ca.keal.varianttap;
 
 import android.content.Context;
+import android.support.annotation.DimenRes;
+import android.util.TypedValue;
+
+import java.util.Random;
 
 final class Util {
   
   private Util() {}
   
-  public static float pxToDp(Context context, float px) {
+  static float pxToDp(Context context, float px) {
     return px / context.getResources().getDisplayMetrics().density;
   }
   
-  public static float dpToPx(Context context, float dp) {
+  static float dpToPx(Context context, float dp) {
     return dp * context.getResources().getDisplayMetrics().density;
+  }
+  
+  static float getFloatResource(Context context, @DimenRes int floatRes) {
+    TypedValue floatValue = new TypedValue();
+    context.getResources().getValue(floatRes, floatValue, true);
+    return floatValue.getFloat();
+  }
+  
+  /**
+   * @return A random float {@code n}, where {@code min <= n <= max}, determined by {@code random}.
+   */
+  static float randomFloatBetween(Random random, float min, float max) {
+    return (max - min) * random.nextFloat() + min;
   }
   
 }
