f9d6b8318572b7945ff4df8f09b12d9794f0860b
Ryan Dancy <ryan@keal.ca>
2018-02-03 11:41:25 -0500

    Add achievements circle button, GPGSCircleButton
    Add AchievementsCircleButton, a circle button extremely similar to
    LeaderboardCircleButton, but which opens the achievements UI instead of
    the leaderboards UI.
    
    Add GPGSCircleButton, an abstract superclass of AchievementsCircleButton
    and LeaderboardCircleButton. It encapsulates most of the functionality
    previously held in LeaderboardCircleButton in order to reuse it for
    AchievementsCircleButton.

---
+++ app/src/main/java/ca/keal/varianttap/ui/circlebutton/GPGSCircleButton.java
@@ -0,0 +1,74 @@
+package ca.keal.varianttap.ui.circlebutton;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.AttrRes;
+import android.util.AttributeSet;
+import android.util.Log;
+
+import ca.keal.varianttap.gpgs.GPGSAction;
+import ca.keal.varianttap.gpgs.GPGSHelperClient;
+import ca.keal.varianttap.gpgs.GPGSHelperService;
+import ca.keal.varianttap.gpgs.GPGSHelperServiceConnection;
+
+/**
+ * The abstract superclass of the circle buttons which are just shortcuts to launch a
+ * {@link GPGSAction} ({@link LeaderboardCircleButton} and {@link AchievementsCircleButton}.)
+ */
+abstract class GPGSCircleButton extends BaseCircleButton implements GPGSHelperClient {
+  
+  private static final String TAG = "GPGSCircleButton";
+  
+  private GPGSHelperService gpgsHelper;
+  private GPGSHelperServiceConnection connection;
+  
+  /** The {@link GPGSAction} performed upon tapping the {@link GPGSCircleButton}. */
+  private final GPGSAction gpgsAction;
+  
+  public GPGSCircleButton(Context context, AttributeSet attrs, @AttrRes int style,
+                          GPGSAction action) {
+    super(context, attrs, style);
+    connection = new GPGSHelperServiceConnection(this);
+    gpgsAction = action;
+  }
+  
+  @Override
+  protected void onAttachedToWindow() {
+    super.onAttachedToWindow();
+    
+    // Bind to the GPGS service
+    Intent intent = new Intent(context, GPGSHelperService.class);
+    context.bindService(intent, connection, Context.BIND_AUTO_CREATE);
+  }
+  
+  @Override
+  protected void onDetachedFromWindow() {
+    super.onDetachedFromWindow();
+    context.unbindService(connection);
+  }
+  
+  @Override
+  public void receiveService(GPGSHelperService service) {
+    gpgsHelper = service;
+  }
+  
+  @Override
+  protected void onClick() {
+    if (!(context instanceof Activity)) {
+      Log.e(TAG, "A GPGSCircleButton can only be used inside an activity. Ignoring click!");
+      return;
+    }
+    
+    Activity activity = (Activity) context;
+    
+    if (gpgsHelper.isConnected()) {
+      gpgsAction.performAction(activity, gpgsHelper.getApiClient());
+    } else {
+      // try to connect, then trigger the gpgsAction
+      gpgsHelper.setActionOnSignIn(activity, gpgsAction);
+      gpgsHelper.connect(activity);
+    }
+  }
+  
+}
--- app/src/main/java/ca/keal/varianttap/ui/circlebutton/LeaderboardCircleButton.java
+++ app/src/main/java/ca/keal/varianttap/ui/circlebutton/LeaderboardCircleButton.java
@@ -1,69 +1,23 @@
 package ca.keal.varianttap.ui.circlebutton;
 
-import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.util.AttributeSet;
-import android.util.Log;
 
 import ca.keal.varianttap.R;
 import ca.keal.varianttap.gpgs.GPGSAction;
-import ca.keal.varianttap.gpgs.GPGSHelperClient;
-import ca.keal.varianttap.gpgs.GPGSHelperService;
-import ca.keal.varianttap.gpgs.GPGSHelperServiceConnection;
 
-public class LeaderboardCircleButton extends BaseCircleButton implements GPGSHelperClient {
-  
-  private static final String TAG = "LeaderboardCircleButton";
-  
-  private GPGSHelperService gpgsHelper;
-  private GPGSHelperServiceConnection connection;
+/**
+ * A {@link GPGSCircleButton} which triggers {@link GPGSAction#ShowLeaderboard}. It consists of
+ * the GPGS leaderboard icon (a crown).
+ */
+public class LeaderboardCircleButton extends GPGSCircleButton {
   
   public LeaderboardCircleButton(Context context) {
     this(context, null);
   }
   
   public LeaderboardCircleButton(Context context, AttributeSet attrs) {
-    super(context, attrs, R.attr.leaderboardCircleButtonStyle);
-    connection = new GPGSHelperServiceConnection(this);
-  }
-  
-  @Override
-  protected void onAttachedToWindow() {
-    super.onAttachedToWindow();
-    
-    // Bind to the GPGS service
-    Intent intent = new Intent(context, GPGSHelperService.class);
-    context.bindService(intent, connection, Context.BIND_AUTO_CREATE);
-  }
-  
-  @Override
-  protected void onDetachedFromWindow() {
-    super.onDetachedFromWindow();
-    context.unbindService(connection);
-  }
-  
-  @Override
-  public void receiveService(GPGSHelperService service) {
-    gpgsHelper = service;
-  }
-  
-  @Override
-  protected void onClick() {
-    if (!(context instanceof Activity)) {
-      Log.e(TAG, "Why are you using LeaderboardCircleButton outside of an Activity? "
-          + "Ignoring click!");
-      return;
-    }
-    
-    Activity activity = (Activity) context;
-    
-    if (gpgsHelper.isConnected()) {
-      GPGSAction.ShowLeaderboard.performAction(activity, gpgsHelper.getApiClient());
-    } else {
-      gpgsHelper.setActionOnSignIn(activity, GPGSAction.ShowLeaderboard);
-      gpgsHelper.connect(activity);
-    }
+    super(context, attrs, R.attr.leaderboardCircleButtonStyle, GPGSAction.ShowLeaderboard);
   }
   
 }
